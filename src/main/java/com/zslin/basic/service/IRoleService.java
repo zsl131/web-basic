f7d33c5fa08e95274cb03300e62f16e8af48b411
zsl131 <398986099@qq.com>
2016-10-27 00:28:17 +0800

    新版本后台，完善权限管理功能，可根据业务扩展

---
+++ src/main/java/com/zslin/basic/service/IRoleService.java
@@ -0,0 +1,16 @@
+package com.zslin.basic.service;
+
+import com.zslin.basic.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+/**
+ * Created by 钟述林 393156105@qq.com on 2016/10/19 9:59.
+ */
+public interface IRoleService extends JpaRepository<Role, Integer> {
+
+    @Query("SELECT rm.mid FROM RoleMenu rm WHERE rm.rid=?1")
+    List<Integer> listRoleMenuIds(Integer roleId);
+}
